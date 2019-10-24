0866470a24cbe6de35b5896c56c065f80c61545a
matu <matu.0@hotmail.com>
2019-10-24 20:42:03 -0300

    Borre las cosas que no van

--- freertos_statechart/Debug/example/src/src-gen/Prefix.d
+++
@@ -1,9 +0,0 @@
-example/src/src-gen/Prefix.o example/src/src-gen/Prefix.d: \
- ../example/src/src-gen/Prefix.c ../example/src/src-gen/sc_types.h \
- ../example/src/src-gen/Prefix.h ../example/src/src-gen/PrefixRequired.h
-
-../example/src/src-gen/sc_types.h:
-
-../example/src/src-gen/Prefix.h:
-
-../example/src/src-gen/PrefixRequired.h:
