244c6b2695dff48b43e30bc4910ae4999b6f33ab
matu <matu.0@hotmail.com>
2019-11-07 20:34:43 -0300

    eliminar

--- lpc_chip_43xx/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- C:\Users\Alan\Leo\UBA\Embebidos\TP3_sistemas_embebidos\lpc_chip_43xx\inc/ring_buffer.h \
- C:\Users\Alan\Leo\UBA\Embebidos\TP3_sistemas_embebidos\lpc_chip_43xx\inc/lpc_types.h
-
-C:\Users\Alan\Leo\UBA\Embebidos\TP3_sistemas_embebidos\lpc_chip_43xx\inc/ring_buffer.h:
-
-C:\Users\Alan\Leo\UBA\Embebidos\TP3_sistemas_embebidos\lpc_chip_43xx\inc/lpc_types.h:
